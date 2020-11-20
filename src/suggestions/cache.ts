624d31be825f1a81f3aefc52bbb8272f18ec89ef
Timothée Clain <timothee.clain@gmail.com>
2020-11-20 11:13:02 -0500

    fix: remove useless cache middleware

--- src/suggestions/cache.ts
+++ src/suggestions/cache.ts
@@ -1,18 +1,5 @@
-import { Handler } from "express";
 import { fromCacheOr } from "../cache";
 import { Suggester, SuggestionsResults } from "./types";
-
-export const cachedSuggestMiddleware = (suggestHandler: Handler): Handler => (
-  req,
-  res,
-  next
-) => {
-  // we use the url as our cache key !
-  return fromCacheOr<SuggestionsResults>(
-    req.url,
-    suggestHandler(req, res, next)
-  );
-};
 
 export const cachedSuggester = (suggester: Suggester): Suggester => (
   db,
